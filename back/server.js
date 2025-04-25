aa902988f426bca7c36620258d46c623f8ac5d5a
26987087 <soussichayma@gmail.com>
2025-04-25 11:07:42 +0200

    Projet local initial

---
+++ back/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = 3000;
+
+// Autoriser les requêtes CORS
+app.use(cors());
+
+app.get("/api/message", (req, res) => {
+  res.json({ message: "Bonjour depuis le serveur ! 👋" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Serveur lancé sur http://localhost:${PORT}`);
+});
